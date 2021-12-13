aaa38f48c940a04836d086e0f0d44f75e9b55ffc
Rahul Siyanwal <rsiyanwal@gmail.com>
2021-12-13 18:39:05 +0530

    Create web_config.py
    This is the initial config file. It has the urls of the website which is to be scraped.

---
+++ src/web_config.py
@@ -0,0 +1,17 @@
+# -- Config File --
+"""
+Links
+"""
+# -- Economic times --
+ECONOMIC_TIMES = "https://economictimes.indiatimes.com/archive.cms"
+ECONOMIC_TIMES_TINY = "economictimes.indiatimes.com"
+ECONOMIC_TIMES_PREFIX = "https://economictimes.indiatimes.com"
+
+# -- OUTPUT --
+output = "output/"
+
+# -- ChromeDriver --
+CHROMEDRIVER = "D:/chromedriver/chromedriver.exe"
+
+# -- All archive links --
+ARCHIVE_LINKS = "D:/Projects/Web Scraper/output/all-archive-links.csv"
